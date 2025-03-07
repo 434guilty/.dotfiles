a0b800a0b082979e3221ebae4d3c650bd607a731
m <musabmohamm@gmail.com>
2025-03-07 08:14:57 +0300

    update

--- hp-14/configuration.nix
+++ hp-14/configuration.nix
@@ -163,7 +163,6 @@
   hardware.graphics = {
     enable = true;
     extraPackages = with pkgs; [
-      vpl-gpu-rt # for newer GPUs on NixOS >24.05 or unstable
       intel-media-driver
     ];
   };
