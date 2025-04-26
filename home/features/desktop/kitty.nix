0605e3e964b9093ef3b2f7cf4e9c0c88666ab424
m <musabmohamm@gmail.com>
2025-04-26 20:37:41 +0300

    Enhance Kitty Tab Bar Appearance

--- home/features/desktop/kitty.nix
+++ home/features/desktop/kitty.nix
@@ -19,6 +19,10 @@ in {
         cursor_trail = 3;
         enable_audio-bell = "no";
         cursor_text_color = "background";
+        tab_bar_edge = "bottom";
+        tab_bar_style = "powerline";
+        tab_powerline_style = "slanted";
+        tab_title_template = "{title}{' :{}:'.format(num_windows) if num_windows > 1 else ''}";
       };
     };
   };
