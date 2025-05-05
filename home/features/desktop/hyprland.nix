8f6bfc51c69e499defae6ef91e9dbccaa51b85c0
m <musabmohamm@gmail.com>
2025-05-05 05:40:56 +0300

    fixed uswm env

--- home/features/desktop/hyprland.nix
+++ home/features/desktop/hyprland.nix
@@ -20,12 +20,12 @@
   home.file = {
     ".config/uwsm/env" = {
       text = ''
-        export GDK_BACKEND, wayland, x11
-        export CLUTTER_BACKEND, wayland
-        export QT_QPA_PLATFORM=wayland;xcb
-        export QT_WAYLAND_DISABLE_WINDOWDECORATION, 1
-        export QT_AUTO_SCREEN_SCALE_FACTOR, 1
-        export MOZ_ENABLE_WAYLAND, 1
+        export GDK_BACKEND='wayland,x11,*'
+        export QT_QPA_PLATFORM='wayland;xcb'
+        export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
+        export QT_AUTO_SCREEN_SCALE_FACTOR=1
+        export MOZ_ENABLE_WAYLAND=1
+        export ELECTRON_OZONE_PLATFORM_HINT=auto
       '';
       executable = false;
     };
@@ -44,6 +44,7 @@
       exec-once = [
         #"dbus-update-activation-environment --all --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
         #"systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
+        "uwsm finalize HYPRLAND_INSTANCE_SIGNATURE HYPRLAND_CMD"
         "swww-daemon"
         "killall -q .waybar-wrapped;sleep .5 && waybar"
         "killall -q .swaync-wrapped;sleep .5 && uwsm app -- swaync"
